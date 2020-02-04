8d385abab493826127429e942632408235a4d921
Dharmveer Baloda <dharmveer.baloda@erosnow.com>
2020-02-04 12:11:24 +0530

    changes

---
+++ blob_storage.py
@@ -0,0 +1,143 @@
+import os
+from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobProperties
+from azure.identity import ClientSecretCredential
+from azure.core.exceptions import ResourceExistsError
+
+class FileName:
+
+    def __init__(self, filename):
+        self.ext = None
+        self.base_path = None
+        self.filename = os.path.basename(filename)
+
+    def parse_filename(self):
+        self.base_path, self.ext = os.path.splitext(self.filename)
+
+
+
+class LocalFile(FileName):
+    local_storge_path = "/Users/dharmveer.baloda/workspace/az-transcoding-poc/downloads"
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.parse_filename()
+        self._size = 0
+
+    @property
+    def absolute_path(self):
+        return os.path.join(self.local_storge_path, self.filename)
+
+    @property
+    def size(self):
+        if self._ssize:
+            return self.size
+        try:
+            self._size = os.stat(self.path).st_size
+        except Exception as exception:
+            pass
+        return self._size
+
+    def delete(self):
+        os.remove(self.filename)
+
+
+# class AzureUploadFile(LocalFile):
+#     def __init__(self, filename, container_name=None, blob_name=None):
+#         super().__init__(filename)
+#         self.container_name = container_name
+#         self.blob_name = blob_name
+
+
+class BlobStorage:
+    URL = "https://{storage_account_name}.blob.core.windows.net/"
+
+    @property
+    def resource_url(self):
+        return self.URL.format(storage_account_name=self.storage_account_name)
+
+    def __init__(self, storage_account_name, access_key, container_name=None, blob_name=None):
+        self.access_key = access_key
+
+        self.storage_account_name = storage_account_name
+        self.container_name = container_name
+        self.blob_name = blob_name
+
+        self._blob_service_client = None
+        self._container_client = None
+        self._blob_client = None
+
+        self.properties = {}
+
+        self.blob_service_client()
+        self.blob_client()
+        self.blob_properties()
+
+    def blob_service_client(self):
+        if not self._blob_service_client:
+            self._blob_service_client = BlobServiceClient(
+                account_url=self.resource_url,
+                credential=self.access_key
+            )
+        return self._blob_service_client
+
+    def blob_client(self):
+        try:
+            self._blob_client = self._blob_service_client.get_blob_client(
+                self.container_name, self.blob_name
+            )
+        except Exception as exception:
+            raise exception
+        return self._blob_client
+
+    def blob_properties(self):
+        if not self.properties:
+            try:
+                self.properties = self.blob_client().get_blob_properties()
+            except Exception as exception:
+                pass
+        return self.properties
+
+    def delete_blob(self, container_name, blob_name):
+        try:
+            self.blob_client(container_name, blob_name).delete_blob()
+        except Exception as exception:
+            raise exception
+
+    def to_local_file(self,filename):
+        filename = filename or self.blob_name
+        if not isinstance(filename, LocalFile):
+            local_file = LocalFile(filename=filename)
+        local_file = LocalFile(filename=filename)
+        return local_file
+
+    def download(self, filename=None):
+        local_file = self.to_local_file(filename=filename)
+        blob_client = self.blob_client()
+        filesize = round(self.properties.get("size", 0)/1024)
+        print(
+            "{} {} size {} Kbps".format(
+                self.container_name, self.blob_name, filesize
+            )
+        )
+        dowloaded_filesize = 0
+        with open(local_file.absolute_path, "wb") as f:
+            dowloaded_filesize += blob_client.download_blob().readinto(f)
+
+            print(
+                "{} {} downloaded {}%".format(
+                    self.container_name, self.blob_name, round(round(dowloaded_filesize/1024)/filesize)*100
+                )
+            )
+
+    def upload(self, filename):
+        local_file=self.to_local_file(filename=filename)
+        try:
+            blob_client = self.blob_client()
+            with open(local_file.absolute_path, "rb") as data:
+                blob_client.upload_blob(data)
+            self.blob_properties()
+        except ResourceExistsError as identifier:
+            raise identifier
+        except Exception as identifier:
+            raise identifier
+
--- poc.py
+++ poc.py
@@ -1,18 +1,24 @@
-from blob_stroage import BlobStorage
+from blob_storage import BlobStorage
 
 
 storage_account_name = "xxxxxxxxxxxxxxxxxxx"
 access_key = "xxxmlzd3aPNMnXSgQwMRhIHTUu3rSs0wfEXMpw6OW0HG5ba2tz04/6dftVHdy1zNpwnTOWu7QjzH/wrpRdhFFg=="
 
+container_name = "transcoder-poc"
+blob_name="datascience-Azure.png"
+filename="datascience-Azure-testing.png"
+
 az_storage = BlobStorage(
     storage_account_name=storage_account_name,
-    access_key=access_key
+    access_key=access_key,
+    container_name=container_name,
+    blob_name="datascience-Azure-testing.png"
 )
 
-containers = az_storage.list_containers()
-container_name = "transcoder-poc"
-az_storage.download(container_name=container_name, blob_name="datascience-Azure.png")
-az_storage.upload(container_name=container_name, filename="datascience-Azure_upload.png")
+# containers = az_storage.list_containers()
 
-
-print(containers)
+blob_properties = az_storage.blob_properties()
+print(blob_properties)
+# print("**********************************************************")
+az_storage.download()
+# az_storage.upload(filename)
