cb54cd43f70fec67d61119493f51d618368b167e
Dharmveer Baloda <dharmveer.baloda@erosnow.com>
2020-02-18 16:22:00 +0530

    config removed to a constant file and constant file is not commited

--- poc.py
+++ poc.py
@@ -1,24 +1,33 @@
-from blob_storage import BlobStorage
+from datetime import datetime, timedelta
+from blob_storage import AzBlobStorage
+from argparse import ArgumentParser
+from constants import *
 
+parser = ArgumentParser(prog='azure file download/upload')
+parser.add_argument('--blob_name', required=True, help="Give blob relative full path to container")
+parser.add_argument('--container_name', required=True, help="Container name is required")
+args = parser.parse_args()
+container_name = args.blob_name
+blob_name = args.blob_name
 
-storage_account_name = "xxxxxxxxxxxxxxxxxxx"
-access_key = "xxxmlzd3aPNMnXSgQwMRhIHTUu3rSs0wfEXMpw6OW0HG5ba2tz04/6dftVHdy1zNpwnTOWu7QjzH/wrpRdhFFg=="
-
-container_name = "transcoder-poc"
-blob_name="datascience-Azure.png"
-filename="datascience-Azure-testing.png"
-
-az_storage = BlobStorage(
+az_storage = AzBlobStorage(
     storage_account_name=storage_account_name,
     access_key=access_key,
     container_name=container_name,
-    blob_name="datascience-Azure-testing.png"
+    blob_name=blob_name
 )
 
 # containers = az_storage.list_containers()
-
 blob_properties = az_storage.blob_properties()
 print(blob_properties)
+started_time = datetime.now()
 # print("**********************************************************")
+
 az_storage.download()
-# az_storage.upload(filename)
+# az_storage.upload()
+
+completed_time = datetime.now()
+print(completed_time, started_time)
+print(
+    "total time taken {}/{} to download is".format(container_name, blob_name), completed_time-started_time
+)
